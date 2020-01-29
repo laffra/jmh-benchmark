aee7e3f923349904e59f0621947563a15faa94e6
Chris Laffra <laffra@uber.com>
2020-01-29 01:54:36 +0100

    Sparse Array using binary search on keys

--- src/main/java/sparsearray/util/SparseArray.java
+++ src/main/java/sparsearray/util/SparseArray.java
@@ -3,28 +3,6 @@ package sparsearray.util;
 import java.util.Map;
 import java.util.TreeMap;
 
-/**
- * {@code SparseArray} maps integers to Objects and, unlike a normal array of
- * Objects, its indices can contain gaps. {@code SparseArray} is intended to be
- * more memory-efficient than a {@code HashMap}, because it avoids auto-boxing
- * keys and its data structure doesn't rely on an extra entry object for each
- * mapping.
- *
- * <p>
- * It is possible to iterate over the {@link #entries() entries} in this
- * container, in which case the entries are returned by ascending key value.
- *
- * <p>
- * Each {@code SparseArray} instance has a {@link #size() size} representing the
- * number of elements currently stored in the container.
- *
- * <p>
- * <strong>Note that this implementation is not synchronized.</strong> If
- * multiple threads access a {@code SparseArray} instance concurrently, and at
- * least one of the threads modifies the array structurally, it must be
- * synchronized externally.
- *
- */
 @SuppressWarnings("unchecked")
 public class SparseArray<T> {
   private static final int CHUNK_SIZE = 256;
@@ -34,9 +12,6 @@ public class SparseArray<T> {
   private int size = 0;
   private int capacity = CHUNK_SIZE;
 
-  /**
-   * Returns the value at {@code index} if present, otherwise {@code null}.
-   */
   public T get(int index) {
     int keyIndex = this.getKeyIndex(index);
     if (this.keys[keyIndex] == index) {
@@ -45,12 +20,6 @@ public class SparseArray<T> {
     return null;
   }
 
-  /**
-   * Set or update the {@code value} at {@code index}.
-   *
-   * If {@code value} is {@code null}, any current value associated with
-   * {@code index} is removed.
-   */
   public void put(int index, T value) {
     int keyIndex = this.getKeyIndex(index);
     if (this.size > 0 && this.keys[keyIndex] == index) {
@@ -60,10 +29,6 @@ public class SparseArray<T> {
     }
   }
 
-  /**
-   * Returns a map view of the sparse array. Iterating over the map should yield
-   * tuples in ascending order of the indices.
-   */
   public Map<Integer, T> entries() {
     Map<Integer, T> map = new TreeMap<Integer, T>();
     for (int n = 0; n < this.size; n++) {
@@ -72,9 +37,6 @@ public class SparseArray<T> {
     return map;
   }
 
-  /**
-   * Count of elements stored in the data structure.
-   */
   public int size() {
     return this.size;
   }
